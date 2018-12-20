ac8abc1a5a1f152a1c31aea41ce3c602c1c6e139
Carlos Villalta <CarlosFindley@protonmail.com>
2018-12-19 21:34:58 -0800

    update

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -18,6 +18,7 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "app/public")));
 
 
 // ==============================================================================
@@ -26,4 +27,15 @@ app.use(express.json());
 // There soutes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ==============================================================================
 
-require("./app/routing/apiRoutes")(app);
+// require("./app/routing/apiRoutes")(app);
+// require("./app/routing/htmlRoutes")(app);
+
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
