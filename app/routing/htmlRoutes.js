4b63e06811895157f339e525eb0db8489a436f4a
Carlos Villalta <CarlosFindley@protonmail.com>
2018-12-30 20:39:44 -0800

    update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -20,16 +20,16 @@ module.exports = function(app) {
   });
 
   // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
+  // app.get("*", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/home.html"));
+  // });
 
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
-  app.get("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "../data/friends"));
-});
+//   app.get("/api/friends", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../data/friends"));
+// });
   
 };
