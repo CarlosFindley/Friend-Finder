205fbf3a2e74449e4498a279ca807b42bbb022c3
Carlos Villalta <CarlosFindley@protonmail.com>
2018-12-31 18:09:48 -0800

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -24,7 +24,50 @@ module.exports = function(app) {
     // ---------------------------------------------------------------------------
 
     app.post("/api/friends", function(req, res) {
-        friendsData.push(req.body);
-        res.json(true);
+        // Test through command that responses are being collected
+        console.log("Res collect", req.body);
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+            matchDiff: 1000
+        };
+
+        var newUserRes = req.body;
+        // Convert each user's results into a simple array of numbers
+        var newScore = newUserRes.score
+        console.log("Res score collect", newScore );
+
+        newUserRes.routeName = newUserRes.name.replace(/\s+/g, "").toLowerCase();
+        var totalDifference = 0;
+
+        // Loop through array
+        for (var i = 0; i < friendsData.length; i++) {
+            totalDifference = 0;
+
+            // Grab newScore from user res and loop through existing scores
+            for (var j = 0; j < friendsData[i].scores[j]; j++) {
+
+                totalDifference += Math.abs(parseInt(newScore[j]) - parseInt(friendsData[i].scores[j]));
+
+                if (totalDifference <= bestMatch.matchDiff) {
+                    bestMatch.name = friendsData[i].name;
+                    bestMatch.photo = friendsData[i].photo;
+                    bestMatch.matchDiff = totalDifference;
+                } else {
+                    console.log("NO MATCH");
+                }
+            }
+        }
+
+
+        
+
+        // compare the difference between current user's scores against those from other users,
+        // question by question. Add up the differences to calculate the totalDifference
+
+        friendsData.push(newUserRes);
+
+        res.json(bestMatch);
     });
 };
